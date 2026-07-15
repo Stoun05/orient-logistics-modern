ff764a265dfbe43750f0cdbb94c36b3142ea7972
Stoun05 <isa1590ag@gmail.com>
2026-07-15 13:19:11 +0500

    Load service detail module after route calculator

--- script.js
+++ script.js
@@ -18,11 +18,13 @@
   loadStyle("phase4.css");
   loadStyle("route-map.css");
   loadStyle("route-search.css");
+  loadStyle("service-details.css");
 
   loadScript("config.js")
     .then(() => loadScript("core.js"))
     .then(() => loadScript("phase3.js"))
     .then(() => loadScript("seo.js"))
     .then(() => loadScript("route-map.js"))
+    .then(() => loadScript("service-details.js"))
     .catch(error => console.error("ORIENT UI load error:", error));
 })();
